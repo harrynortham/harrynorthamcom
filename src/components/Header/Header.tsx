b81c2c58ae0877972edf2c911a90a3135fb59981
Harry Northam <40062105+harrynortham@users.noreply.github.com>
2025-01-21 21:03:04 +0100

    Header and footer components added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div className="header ">
+            <div className="mx-auto max-w-7xl p-4">
+
+                <div className="logo">harrynortham</div>
+
+            </div>
+        </div>
+    )
+}
