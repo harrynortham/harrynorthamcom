2a1b11903287cb878c1c9218ff0b2e2453b75298
Harry Northam <40062105+harrynortham@users.noreply.github.com>
2025-01-22 11:29:13 +0100

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Contact from "@/components/Contact";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <Contact />
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-BFZBM0LK5M" />
     </html>
   );
 }
