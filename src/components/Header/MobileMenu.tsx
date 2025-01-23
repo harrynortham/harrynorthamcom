17a17359b3b162deca0c1eb0ec7b72c354c3fb50
Harry Northam <40062105+harrynortham@users.noreply.github.com>
2025-01-23 20:48:51 +0100

    Added hover states elements

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -24,7 +24,7 @@ export default function MobileMenu() {
 
         <div className="navigation-mobile flex justify-center md:hidden">
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
-                <SheetTrigger className="focus-visible:outline-none">
+                <SheetTrigger className="focus-visible:outline-none hover:bg-slate-100 rounded-md">
                     <IoMenu className="h-7 w-7" />
                     <span className="sr-only">Menu</span>
                 </SheetTrigger>
@@ -37,13 +37,13 @@ export default function MobileMenu() {
                             </SheetDescription>
                         </SheetHeader>
                     </div>
-                    <div className="flex flex-col space-y-4 mt-6">
-                        <Link href="/about" onClick={handleLinkClick}>About</Link>
-                        <Link href="/experience" onClick={handleLinkClick}>Experience</Link>
-                        <Link href="/projects" onClick={handleLinkClick}>Projects</Link>
-                        <Link href="/blog" onClick={handleLinkClick}>Blog</Link>
+                    <div className="flex flex-col space-y-1 mt-6">
+                        <Link className="hover:bg-neutral-100 py-2 px-4" href="/about" onClick={handleLinkClick}>About</Link>
+                        <Link className="hover:bg-neutral-100 py-2 px-4" href="/experience" onClick={handleLinkClick}>Experience</Link>
+                        <Link className="hover:bg-neutral-100 py-2 px-4" href="/projects" onClick={handleLinkClick}>Projects</Link>
+                        <Link className="hover:bg-neutral-100 py-2 px-4" href="/blog" onClick={handleLinkClick}>Blog</Link>
                     </div>
-                    <div className="mt-8"><Link href="/contact" onClick={handleLinkClick}>
+                    <div className="mt-12"><Link href="/contact" onClick={handleLinkClick}>
                         <Button size="lg" className="text-base w-full">Contact <IoMailOutline /></Button>
                     </Link></div>
                     <div
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -65,7 +65,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {/* <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"> */}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100  disabled:pointer-events-none data-[state=open]:bg-secondary focus-visible:outline-none">
+      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100  disabled:pointer-events-none data-[state=open]:bg-secondary focus-visible:outline-none hover:bg-neutral-100">
         <X className="h-6 w-6" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
